f8ada174f8dd46f6e2da9fa005320cd70cb8ed32
Denis <denmagg.work@gmail.com>
2022-06-20 02:34:48 +0300

    fixed textFields text alignment

--- RestaurantAPP/MenuEditorModule/View/CustomTextField.swift
+++ RestaurantAPP/MenuEditorModule/View/CustomTextField.swift
@@ -13,6 +13,8 @@ class CustomTextField: UITextField {
         static let cornerRadius: CGFloat = 10
         static let borderColor = CGColor(red: 39/255, green: 93/255, blue: 113/255, alpha: 1.0)
         static let borderWidth: CGFloat = 1.0
+        static let placeholderPositionInset: CGFloat = 18
+        static let textPositionInset: CGFloat = 18
     }
 
     required init(placeholder: String) {
@@ -28,6 +30,14 @@ class CustomTextField: UITextField {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: Consts.placeholderPositionInset, y: 0, width: bounds.width, height: bounds.height)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRect(x: Consts.textPositionInset, y: 0, width: bounds.width, height: bounds.height)
     }
 
 }
